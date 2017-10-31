d73c6579919af35d1ef4fe92baab4e91af0164ff
Dan Wasmer <wasmer.dan@gmail.com>
2017-10-30 19:55:14 -0500

    refresh repo to remove TS compiled Javascript and js.map files

--- src/app/shared/models/EventListItem.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EventListItem = /** @class */ (function () {
-    function EventListItem() {
-    }
-    return EventListItem;
-}());
-exports.EventListItem = EventListItem;
-//# sourceMappingURL=EventListItem.js.map
