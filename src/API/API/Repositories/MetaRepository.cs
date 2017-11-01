a50ace7a37ed57cdd15b34a1be88bf865a3b5f10
Dan Wasmer <wasmer.dan@gmail.com>
2017-10-31 22:17:40 -0500

    implemeted meta repository

--- src/API/API/Repositories/MetaRepository.cs
+++ src/API/API/Repositories/MetaRepository.cs
@@ -1,3 +1,4 @@
+using API.DataAccess;
 using API.Interfaces;
 using API.Models;
 using System;
@@ -7,31 +8,54 @@ using System.Web;
 
 namespace API.Repositories
 {
-  public class MetaRepository : IMetaRepository
+  public class MetaRepository : IMetaRepository, IDisposable
   {
+
+    private AdminDBContext _db = new AdminDBContext();
+
     public void CreateMeta(Meta entity)
     {
-      throw new NotImplementedException();
+      _db.Metas.Add(entity);
+      _db.SaveChanges();
     }
 
     public void DeleteMeta(Guid id)
     {
-      throw new NotImplementedException();
+      var meta = new Meta() { MetaID = id };
+      _db.Metas.Attach(meta);
+      _db.Metas.Remove(meta);
+      _db.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+      if (_db != null)
+      {
+        _db.Dispose();
+        _db = null;
+      }
     }
 
     public IEnumerable<Meta> GetAllMetas()
     {
-      throw new NotImplementedException();
+      return (from m in _db.Metas select m).ToArray();
     }
 
     public Meta GetById(Guid id)
     {
-      throw new NotImplementedException();
+      return (from m in _db.Metas where m.MetaID == id select m).FirstOrDefault();
     }
 
     public void UpdateMeta(Guid id, Meta entity)
     {
-      throw new NotImplementedException();
+      var currentEntity = _db.Metas.Find(id);
+      if (currentEntity == null)
+      {
+        return;
+      }
+
+      _db.Entry<Meta>(currentEntity).CurrentValues.SetValues(entity);
+      _db.SaveChanges();
     }
 
   }
