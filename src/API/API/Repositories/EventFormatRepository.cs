ac7ecd09df8521b6d18c260eef6df8c609862e24
Dan Wasmer <wasmer.dan@gmail.com>
2017-10-31 21:50:40 -0500

    Fleshed out EventFormat Repository Added Unity DI container Added Swagger page

--- src/API/API/Repositories/EventFormatRepository.cs
+++ src/API/API/Repositories/EventFormatRepository.cs
@@ -1,3 +1,4 @@
+using API.DataAccess;
 using API.Interfaces;
 using API.Models;
 using System;
@@ -7,31 +8,55 @@ using System.Web;
 
 namespace API.Repositories
 {
-  public class EventFormatRepository : IEventFormatRepository
+  public class EventFormatRepository : IEventFormatRepository, IDisposable
   {
+
+    private AdminDBContext _db = new AdminDBContext();    
+
     public void CreateEvent(EventFormat entity)
     {
-      throw new NotImplementedException();
+      _db.EventFormats.Add(entity);
+      _db.SaveChanges();
     }
 
     public void DeleteEvent(Guid id)
     {
-      throw new NotImplementedException();
+      EventFormat customer = new EventFormat() { FormatID = id };
+      _db.EventFormats.Attach(customer);
+      _db.EventFormats.Remove(customer);
+      _db.SaveChanges();
+
+    }
+
+    public void Dispose()
+    {
+      if (_db != null)
+      {
+        _db.Dispose();
+        _db = null;
+      }
     }
 
     public IEnumerable<EventFormat> GetAllEvents()
     {
-      throw new NotImplementedException();
+      return (from ef in _db.EventFormats select ef).ToArray();
     }
 
     public EventFormat GetById(Guid id)
     {
-      throw new NotImplementedException();
+      return (from ef in _db.EventFormats where ef.FormatID == id select ef).FirstOrDefault();
     }
 
     public void UpdateEvent(Guid id, EventFormat entity)
     {
-      throw new NotImplementedException();
+      var currentEntity = _db.EventFormats.Find(id);
+      if (currentEntity == null)
+      {
+        return;
+      }
+
+      _db.Entry<EventFormat>(currentEntity).CurrentValues.SetValues(entity);
+      _db.SaveChanges();
     }
   }
 }
