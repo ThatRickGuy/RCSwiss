47deaf85c13258e66e86d3fb5118db9859c6a3a6
Dan Wasmer <wasmer.dan@gmail.com>
2017-10-31 22:12:47 -0500

    implemented ScenarioRepository

--- src/API/API/Repositories/ScenarioRepository.cs
+++ src/API/API/Repositories/ScenarioRepository.cs
@@ -1,3 +1,4 @@
+using API.DataAccess;
 using API.Interfaces;
 using API.Models;
 using System;
@@ -7,31 +8,54 @@ using System.Web;
 
 namespace API.Repositories
 {
-  public class ScenarioRepository : IScenarioRepository
+  public class ScenarioRepository : IScenarioRepository, IDisposable
   {
+
+    private AdminDBContext _db = new AdminDBContext();
+
     public void CreateScenario(Scenario entity)
     {
-      throw new NotImplementedException();
+      _db.Scenarios.Add(entity);
+      _db.SaveChanges();
     }
 
     public void DeleteScenario(Guid id)
     {
-      throw new NotImplementedException();
+      Scenario scenario = new Scenario() { ScenarioID = id };
+      _db.Scenarios.Attach(scenario);
+      _db.Scenarios.Remove(scenario);
+      _db.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+      if (_db != null)
+      {
+        _db.Dispose();
+        _db = null;
+      }
     }
 
     public IEnumerable<Scenario> GetAllScenarios()
     {
-      throw new NotImplementedException();
+      return (from s in _db.Scenarios select s).ToArray();
     }
 
     public Scenario GetById(Guid id)
     {
-      throw new NotImplementedException();
+      return (from s in _db.Scenarios where s.ScenarioID == id select s).FirstOrDefault();
     }
 
     public void UpdateScenario(Guid id, Scenario entity)
     {
-      throw new NotImplementedException();
+      var currentEntity = _db.Scenarios.Find(id);
+      if (currentEntity == null)
+      {
+        return;
+      }
+
+      _db.Entry<Scenario>(currentEntity).CurrentValues.SetValues(entity);
+      _db.SaveChanges();
     }
 
   }
