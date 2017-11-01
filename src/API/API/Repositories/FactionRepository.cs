d883e1acbd8f39568c02d16810cc86f67cf6810d
Dan Wasmer <wasmer.dan@gmail.com>
2017-10-31 22:21:53 -0500

    implemented FactionRepository

--- src/API/API/Repositories/FactionRepository.cs
+++ src/API/API/Repositories/FactionRepository.cs
@@ -1,3 +1,4 @@
+using API.DataAccess;
 using API.Interfaces;
 using API.Models;
 using System;
@@ -7,31 +8,54 @@ using System.Web;
 
 namespace API.Repositories
 {
-  public class FactionRepository : IFactionRepository
+  public class FactionRepository : IFactionRepository, IDisposable
   {
+
+    private AdminDBContext _db = new AdminDBContext();
+
     public void CreateFaction(Faction entity)
     {
-      throw new NotImplementedException();
+      _db.Factions.Add(entity);
+      _db.SaveChanges();
     }
 
     public void DeleteFaction(Guid id)
     {
-      throw new NotImplementedException();
+      var faction = new Faction() { FactionID = id };
+      _db.Factions.Attach(faction);
+      _db.Factions.Remove(faction);
+      _db.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+      if (_db != null)
+      {
+        _db.Dispose();
+        _db = null;
+      }
     }
 
     public IEnumerable<Faction> GetAllFactions()
     {
-      throw new NotImplementedException();
+      return (from f in _db.Factions select f).ToArray();
     }
 
     public Faction GetById(Guid id)
     {
-      throw new NotImplementedException();
+      return (from f in _db.Factions where f.FactionID == id select f).FirstOrDefault();
     }
 
     public void UpdateFaction(Guid id, Faction entity)
     {
-      throw new NotImplementedException();
+      var currentEntity = _db.Factions.Find(id);
+      if (currentEntity == null)
+      {
+        return;
+      }
+
+      _db.Entry<Faction>(currentEntity).CurrentValues.SetValues(entity);
+      _db.SaveChanges();
     }
 
   }
